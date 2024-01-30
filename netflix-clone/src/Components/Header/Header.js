8fdf98487decb4c2fcc3f359a98d4d8d46598fac
mussietesfay <154098279+mussietesfay@users.noreply.github.com>
2024-01-29 23:51:51 -0800

    add html

---
+++ netflix-clone/src/Components/Header/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import "./header.css"
+const Header = () => {
+  return (
+    <div className='header_outer_container'>
+        <div className="header_container">
+        <div className="header_left">
+            <ul>
+                <li>netflix</li>
+                <li>Home</li>
+                <li>Tv Shows</li>
+                <li>Movies</li>
+                <li>New & Popular</li>
+                <li>My List</li>
+                <li>Browse by Languages</li>
+            </ul>
+        </div>
+        <div className="header_right">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Header
