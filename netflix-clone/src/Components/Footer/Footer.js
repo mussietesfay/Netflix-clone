a53117a754187d1f2addfbcac8bcd69216432965
mussietesfay <154098279+mussietesfay@users.noreply.github.com>
2024-02-02 08:15:59 -0800

    header,footer & axios

--- netflix-clone/src/Components/Footer/Footer.js
+++ netflix-clone/src/Components/Footer/Footer.js
@@ -1,8 +1,69 @@
 import React from 'react'
-
+import './Footer.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 const Footer = () => {
   return (
-    <div >footer</div>
+    <div>
+      <div className='Footer_outer_container'>
+        <div className='Footer_inner_container'>
+          <div className='Footer_container'>
+            <FacebookIcon />
+            <InstagramIcon />
+            <TwitterIcon/>
+            <YouTubeIcon />
+          </div>
+
+
+          <div className='Footer_data'>
+            <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookies Preferences</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>Gift Cards</li>
+              <li>Term of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+
+
+          <div className='Service_Code'>
+            <p>
+              Service Code
+            </p>
+          </div>
+          </div>
+
+          <div className='Copy_Write'>
+            &copy;1997-2021 Netflix, Inc.
+            </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- netflix-clone/src/Components/Header/Header.js
+++ netflix-clone/src/Components/Header/Header.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import "./header.css"
+import Netflixlogo from'../../assets/images/Netflix-logo.jpg'
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const Header = () => {
   return (
+    
     <div className='header_outer_container'>
-        <div className="header_container">
-        <div className="header_left">
-            <ul>
-                <li>netflix</li>
-                <li>Home</li>
-                <li>Tv Shows</li>
-                <li>Movies</li>
-                <li>New & Popular</li>
-                <li>My List</li>
-                <li>Browse by Languages</li>
-            </ul>
-        </div>
-        <div className="header_right">
-            <ul>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
-    </div>
-    </div>
+    <div className='header_container'>
+      <div className='header_left'>
+          <ul>
+              <li><img src={Netflixlogo} alt="Netflix Logo" width="100" /></li>
+              <li>Home</li>
+              <li>TvShows</li>
+              <li>Movies</li>
+              <li>Latest</li>
+              <li>MyList</li>
+              <li>Browse by Language</li>
+          </ul>
+      </div>
+
+      <div className='header_right'>
+          <ul>
+          <li><SearchIcon /></li>
+              <li><NotificationsNoneIcon /></li>
+              <li><AccountBoxIcon /></li>
+              <li><ArrowDropDownIcon /></li>
+          </ul>
+
+          </div>
+      </div>
+
+
+      </div>
+   
   )
 }
 
