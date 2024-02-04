759cb36b06ea62ba4786c58c675789c546835e70
mussietesfay <154098279+mussietesfay@users.noreply.github.com>
2024-02-04 08:11:36 -0800

    html,css and others

---
+++ netflix-clone/src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RowList = () => {
+  return (
+    <div>RowList</div>
+  )
+}
+
+export default RowList
--- netflix-clone/src/utils/requests.js
+++ netflix-clone/src/utils/requests.js
@@ -1,8 +1,8 @@
 const API_KEY=process.env.REACT_APP_API_KEY;
 
 const requests = {
-    // fetchTrending: `Trending/all/week?api_key=${API_KEY}&language=en-US`,
-    fetchTrending: `Trending/all/wee?api_key=${API_KEY}&language=en-US`,
+    
+    fetchTrending: `/Trending/all/wee?api_key=${API_KEY}&language=en-US`,
     fetchNetflixOrgionals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRatedMovies: `/movie/top_related?api_key=${API_KEY}&language=en-US`,
     fetchActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
